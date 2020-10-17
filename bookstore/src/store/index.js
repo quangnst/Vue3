18fce1a66db761aac80f58a5cce42ac4cd0fc305
quangnst <quangnst@gmail.com>
2020-10-17 16:47:08 +0700

    add store

---
+++ bookstore/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+    //   isLoggedIn: false
+    };
+  },
+  mutations: {
+    // setIsLoggedIn(state, payload) {
+    //   state.isLoggedIn = payload;
+    // }
+  },
+});
+
+export default store;
